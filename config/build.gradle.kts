747dfbc34532b13bf6b73c92a758da6995ce0b54
javaria <imdangun@gmail.com>
2025-11-18 22:56:36 +0900

    5.1 config + git

---
+++ config/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.msa"
+version = "0.0.1-SNAPSHOT"
+description = "config"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.0"
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
