aa064d750467784ec1e1a423a3e2ca5b8269d025
javaria <imdangun@gmail.com>
2025-12-06 18:00:59 +0900

    7

--- service/company/build.gradle.kts
+++ service/company/build.gradle.kts
@@ -14,5 +14,5 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
 
 	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
-	implementation("io.github.resilience4j:resilience4j-spring-boot3:2.2.0")
+	implementation("io.github.resilience4j:resilience4j-spring-boot3")
 }
